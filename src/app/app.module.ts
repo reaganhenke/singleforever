4eef142fc2dcedd43b0e115c06118039ee879db9
Reagan Henke <reaganhenke@Reagans-MBP.lan>
2019-06-20 22:01:58 -0400

    search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,14 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  showSearch = false;
+  searchForm = new FormGroup({
+    searchInput: new FormControl({value: '', disabled: true})
+  });
+  togglingSearch = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  test() {
-    console.log('search was clicked');
+  toggleSearch() {
+    if (!this.togglingSearch) {
+      if (this.showSearch) {
+        this.showSearch = false;
+        this.togglingSearch = true;
+        setTimeout(() => {
+          this.searchForm.get('searchInput').disable();
+          this.togglingSearch = false;
+        }, 500);
+      } else {
+        this.searchForm.get('searchInput').enable();
+        this.togglingSearch = true;
+        setTimeout(() => { this.showSearch = true; }, 10);
+        setTimeout(() => {
+          document.getElementById('searchInput').focus();
+          this.togglingSearch = false;
+        }, 500);
+      }
+    }
   }
 
 }
