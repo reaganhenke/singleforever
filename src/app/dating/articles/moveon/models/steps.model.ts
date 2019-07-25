8a1d09bac224e55918810c36d1c395be1fb2e678
Reagan Henke <reaganhenke@Reagans-MBP.lan>
2019-07-25 07:20:31 -0400

    Add moveon content

--- src/app/dating/articles/moveon/models/steps.model.ts
+++ src/app/dating/articles/moveon/models/steps.model.ts
@@ -8,14 +8,52 @@ export const steps = [
   {
     title: '1. Listen to sad music',
     imgUrl: 'assets/moveon/music.svg',
-    content: 'You’ll never be able to listen to certain albums again without crying.'
+    content: `Sometimes, you just need to cry it out.
+      You need time to grieve. Sing along to the songs
+      you had with him while crying into a pint of ice cream.
+      Just a warning though, you’ll never be able to listen to
+      the album Blonde by Frank Ocean ever, ever again.`
   }, {
     title: '2. Cleanse',
     imgUrl: 'assets/moveon/cleanse.svg',
-    content: 'Block him on everything.'
+    content: `Make a clean break. Block him on everything.
+      That’s right, don’t just unfriend him, block him. Block his number,
+      block his facebook, block his twitter. Don’t forget to set up an email
+      filter for all his addresses too, because he will try to follow you
+      across the country if he thinks there’s a chance you’ll still talk to him.`
   }, {
     title: '3. Travel',
     imgUrl: 'assets/moveon/travel.svg',
-    content: 'Go all the way to Belize, and then only talk about your ex. '
+    content: `When everything reminds you of him, sometimes you just
+    need to get away! Grab a girlfriend and a suitcase and pick a place,
+    any place. Do what I did, go all the way to Belize, and then spend
+    the entire time in paradise only talking about your ex. To this day,
+    my travel buddy still won’t speak to me.`
+  }, {
+    title: '4. Try something new',
+    imgUrl: 'assets/moveon/new.svg',
+    content: `You know all the things you used to do together that
+    remind you too much of him now? Try something totally new!
+    Never been a big social media user? Start a finstagram!
+    Accidentally see a picture of him with his new girlfriend.
+    Cry a lot! Delete it immediately!`
+  }, {
+    title: '5. Rebound',
+    imgUrl: 'assets/moveon/rebound.svg',
+    content: `You know what they say, the best way to get over someone
+    is to get under someone new. Redownload tinder and try out all those
+    dirty pickup lines you’ve heard about. Go home with a stranger.
+    Try not to say your ex’s name by mistake. Try not to say "I love you"
+    by accident. Try not to cry.`
+  }, {
+    title: '6. Give it time',
+    imgUrl: 'assets/moveon/time.svg',
+    content: `Eventually, time heals all wounds. It’ll get easier. At least,
+    that’s what I keep telling myself. Except it’s already been two and a half
+    years. Any day now though. `
+  }, {
+    title: '7. I don\'t know',
+    imgUrl: 'assets/moveon/dontknow.svg',
+    content: `...If you figure it out though, can you tell me?`
   }
 ];
