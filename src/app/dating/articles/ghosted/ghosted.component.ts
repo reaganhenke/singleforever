2c7c38ea1745b603eda4a526d4706fcfce3c4fc4
Reagan Henke <reaganhenke@Reagans-MBP.lan>
2019-07-24 21:19:51 -0400

    ghosted

--- src/app/dating/articles/ghosted/ghosted.component.ts
+++ src/app/dating/articles/ghosted/ghosted.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { texts } from './models/texts.model';
 
 @Component({
   selector: 'app-ghosted',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ghosted.component.scss']
 })
 export class GhostedComponent implements OnInit {
+  scrolled = 0;
+  windowHeight = 0;
+  scrollHeight = 0;
+  numTexts = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.windowHeight = document.getElementById('texts').clientHeight;
+    this.scrollHeight = document.getElementById('texts').scrollHeight;
   }
 
+  onScroll() {
+    this.scrolled = document.getElementById('texts').scrollTop;
+
+    // if at the bottom
+    if (this.scrolled === (this.scrollHeight - this.windowHeight)) {
+
+      const newText = document.createElement('span');
+      newText.textContent = texts[(Math.floor(Math.random() * texts.length))];
+
+      setTimeout(() => {
+        document.getElementById('her').appendChild(newText);
+        this.scrollHeight = document.getElementById('texts').scrollHeight;
+        this.numTexts = document.getElementById('her').childNodes.length;
+      }, 500);
+    }
+  }
 }
---
+++ src/app/dating/articles/ghosted/models/texts.model.ts
@@ -0,0 +1,14 @@
+export const texts = [
+  'what are you talking about?',
+  'oh wait, seriously?',
+  'this is it?',
+  'seriously, you\'re done?',
+  'just like that?',
+  'I don\'t believe this',
+  'give me another chance!',
+  'i thought things were going really well between us',
+  'can you tell me what i did wrong?',
+  'fuck you',
+  'i\'m sorry',
+  'so.. that\'s a no on dinner tonight?'
+];
