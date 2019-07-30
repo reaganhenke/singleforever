0678ae8884bb54d9a3ac9af20c9e0562572c9b69
Reagan Henke <reaganhenke@Reagans-MBP.lan>
2019-07-29 21:22:04 -0400

    random articles on front page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  idxOne = '';
+  idxTwo = 'whodate';
+  templateIds = ['whodate', 'redflags', 'moveon', 'ghosted', 'mealprep', 'morning'];
 
   constructor() { }
 
   ngOnInit() {
-  }
+    const numOne = Math.floor(Math.random() * 6);
+    let numTwo: number;
 
+    do {
+      numTwo = Math.floor(Math.random() * 6);
+    } while (numOne === numTwo);
+
+    this.idxOne = this.templateIds[numOne];
+    this.idxTwo = this.templateIds[numTwo];
+  }
 }
